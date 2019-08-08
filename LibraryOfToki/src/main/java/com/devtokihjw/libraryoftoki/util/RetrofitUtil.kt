ebfca16cea8821ad457f6dc0813cbafe420d2c55
Huang Jhih Wei <devtoki@soft-world.com.tw>
2019-08-08 16:52:37 +0800

    修改Retrofit

--- LibraryOfToki/src/main/java/com/devtokihjw/libraryoftoki/util/RetrofitUtil.kt
+++ LibraryOfToki/src/main/java/com/devtokihjw/libraryoftoki/util/RetrofitUtil.kt
@@ -5,15 +5,7 @@ import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
 import java.util.concurrent.TimeUnit
-
-fun String.buildRetrofit() = Retrofit.Builder()
-        .baseUrl(this)
-        .client(buildClient())
-        .addConverterFactory(ScalarsConverterFactory.create())
-        .build()
 
 private fun buildClient() = OkHttpClient.Builder()
         .readTimeout(30, TimeUnit.SECONDS)
