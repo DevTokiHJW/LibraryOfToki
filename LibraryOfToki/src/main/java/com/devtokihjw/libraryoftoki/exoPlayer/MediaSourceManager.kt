0ebab19f61d77a4584ea7ca6b7971252b1ea7c18
Huang Jhih Wei <devtoki@soft-world.com.tw>
2019-08-20 16:50:12 +0800

    first commit

---
+++ LibraryOfToki/src/main/java/com/devtokihjw/libraryoftoki/exoPlayer/MediaSourceManager.kt
@@ -0,0 +1,10 @@
+package com.devtokihjw.libraryoftoki.exoPlayer
+
+import com.google.android.exoplayer2.source.MediaSource
+
+class PlayerManager {
+
+    private val list = arrayListOf<MediaSource>()
+
+    private var index = 0
+}
---
+++ LibraryOfToki/src/main/java/com/devtokihjw/libraryoftoki/util/EXOPlayerUtil.kt
@@ -0,0 +1,2 @@
+package com.devtokihjw.libraryoftoki.util
+
